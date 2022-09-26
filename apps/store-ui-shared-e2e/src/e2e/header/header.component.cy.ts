cea956f298ff1db00ae20d441984eab01345e01c
Simon Coakley <SCoakley@slb.com>
2022-09-26 14:16:12 +0100

    Lab 11

--- apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
+++ apps/store-ui-shared-e2e/src/e2e/header/header.component.cy.ts
@@ -1,8 +1,8 @@
 describe('store-ui-shared', () => {
   beforeEach(() =>
-    cy.visit('/iframe.html?id=headercomponent--primary&args=title;')
+    cy.visit('/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard;')
   );
   it('should render the component', () => {
-    cy.get('bg-hoard-header').should('exist');
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });
